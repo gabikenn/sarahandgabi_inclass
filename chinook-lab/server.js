7414dcfe3a6138fa8462246aa9c79492bbab054e
Sarah T <156830537+SunnieTea@users.noreply.github.com>
2026-04-22 15:01:12 +0000

    coding

--- chinook-lab/server.js
+++ chinook-lab/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { DatabaseSync } = require("node:sqlite");
 
-const db = new DatabaseSync("./chinook.db");
+const db = new DatabaseSync("Chinook_Sqlite.sqlite");
 const app = express();
 app.use(express.json());
 
@@ -15,4 +15,12 @@ app.get('/tables', (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
+});
+
+/* stuff I am adding */
+app.get('/artists', (req, res) => {
+    const stmt = db.prepare(
+        "SELECT name FROM artist WHERE type='table' ORDER BY name"
+    );
+    res.json(stmt.all());
 });
